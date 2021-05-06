fe43240fb30b941e001683c1d3a52d60c0c0f63e
doowon13 <endnjs2007@gmail.com>
2021-05-06 16:28:24 +0900

    확인버튼 > 입력된 문장을 텍스트뷰에 반영

--- app/src/main/java/com/pluto/uikotlinpractice_doowon_20210506/MainActivity.kt
+++ app/src/main/java/com/pluto/uikotlinpractice_doowon_20210506/MainActivity.kt
@@ -9,7 +9,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//    확인 버튼이 눌렸을 때 실행할 이벤트
 
+        okBtn.setOnClickListener {
+//            입력된 내용을 (contentEdt의 text값) 조회하여 변수에 저장한다. (조회한다 = get)
+
+            val inputContent = contentEdt.text.toString()
+//            contentEdt의 text를 String으로
+
+            resultTxt.text = inputContent
+
+        }
 
     }
 }
