533c96b570f657dc371971c9b2744f8571c0e851
doowon13 <endnjs2007@gmail.com>
2021-05-06 16:08:30 +0900

    Initial commit

---
+++ app/src/main/java/com/pluto/uikotlinpractice_doowon_20210506/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pluto.uikotlinpractice_doowon_20210506
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
